d72643de35ddbdcfdb65b9f0c8775e89c8ac431d
qFlensT <zhekos1123@ya.ru>
2024-03-03 23:37:03 +0300

    Refactor BasketView class to remove unnecessary event emission, added event table to README.md

--- src/components/view/Basket/BasketView.ts
+++ src/components/view/Basket/BasketView.ts
@@ -18,19 +18,14 @@ export class BasketView extends Basket {
 
 	public addItem(props: BasketItemViewProps): this {
 		this.__items.push(props);
-		this.events.emit('basket:change', { amount: this.__items.length });
 		return this;
 	}
 
 	public removeItemById(id: string): this {
-		console.log('BasketView.removeItemById, id: ', id);
-
 		const itemIndex = this.__items.findIndex((item) => item.id === id);
 		if (itemIndex !== -1) {
 			this.__items.splice(itemIndex, 1);
 		}
-
-		this.events.emit('basket:change', { amount: this.__items.length });
 
 		return this;
 	}
@@ -38,7 +33,6 @@ export class BasketView extends Basket {
 	public clear(): this {
 		super.clear();
 		this.__items = [];
-		this.events.emit('basket:change', { amount: this.__items.length });
 
 		return this;
 	}
